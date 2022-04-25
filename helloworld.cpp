41886dd408b0e4bbe8ed5d5134f280c7ddf7bb77
Amine Belkoura <mxb135330@utdallas.edu>
2022-04-25 18:58:49 -0500

    test

---
+++ helloworld.cpp
@@ -0,0 +1,17 @@
+// file: helloWorld.cpp
+// author: M. Amine Belkoura
+// date: 02/06/2015
+// purpose: CS3376
+// description:
+//   this prints a welcome message to the screen
+#include "printMessage.h"
+#include <iostream>
+using namespace std;
+int main( )
+{
+     printMessage();
+     return 0;
+
+
+}
+
